f3cad10230fc975a7d29a040cbe47d63547e5d3b
Belen Marquez <110306602+BetMarmen@users.noreply.github.com>
2023-08-02 12:10:41 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//Prueba de Aplicacion de Consola
+String  bienvenido= "Bienvenido al curso de Programacion I";
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine(bienvenido);
+Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.Black;
+
